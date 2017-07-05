fa4f79017dd9e4d127556a710d26b16775015dc8
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-07-05 11:38:52 +0200

    Added some initial Button and TextField tests

--- src/Components/Button/Button.test.js
+++ src/Components/Button/Button.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
 import Button from './Button';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Button />, div);
+describe('Button', () => {
+  it('renders without crashing', () => {
+    shallow(<Button />);
+  });
+
+  it('renders a given text prop', () => {
+    const button = shallow(<Button text='some test text' />);
+
+    expect(button.text()).toEqual('some test text');
+  });
+
+  it('executes a given onclick function on click', () => {
+    const buttonClick = sinon.spy(() => 'test');
+    const button = mount(<Button onClick={buttonClick} />);
+
+    button.simulate('click');
+
+    expect(buttonClick.calledOnce).toEqual(true);
+    expect(buttonClick.firstCall.returnValue).toEqual('test');
+  });
+
+  it('renders disabled given a true disabled prop', () => {
+    const button = mount(<Button text='DISABLED' disabled={true} />);
+
+    expect(button.text()).toEqual('DISABLED');
+    expect(button.props().disabled).toEqual(true);
+  });
+
+  it('renders with correct default props', () => {
+    const button = shallow(<Button />);
+
+    expect(button.props()).toEqual({
+      onClick: undefined,
+      children: 'UNDEFINED BUTTON' // children = button text
+    });
+  });
 });
 
-it('renders a given text prop', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Button text="TESTING THE BUTTON" />, div)
-  expect()
-})
