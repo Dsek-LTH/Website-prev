7593b8e76502b2ce595c74536c67d98ec1d681a7
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-07-05 10:41:38 +0200

    Added css import and refactored Button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import './Button.css';
 
-const Button = ({text = 'UNDEFINED BUTTON', onClick}) => {
+const Button = ({
+  text = 'UNDEFINED BUTTON',
+  onClick
+}) => {
   return (
     <button
       onClick={onClick}
     >
       {text}
     </button>
-  )
-}
+  );
+};
 
 export default Button;
